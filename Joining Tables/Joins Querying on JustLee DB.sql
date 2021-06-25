00a4d66b74c332edeaa923f5e5241bac461c3bfb
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-06-25 18:21:29 -0400

    Joining Tables & Querying  w/ JustLee Books DB
    Making queries in JustLee DB for book store using joins.

--- Joining Tables/Joins Querying on JustLee DB.sql
+++ Joining Tables/Joins Querying on JustLee DB.sql
@@ -1,1 +1,25 @@
+SELECT book.title, auth.lname, auth.fname
+FROM bookauthor ba,
+books book,
+author auth
+where auth.authorid = ba.authorid
+and ba.isbn = book.isbn;
 
+
+select books.TITLE, publisher.name, publisher.phone
+from books join PUBLISHER
+using (pubid);
+
+
+select (NVL(to_char(orders.shipdate) ,'Not Shipped')) as "Shipment Status",
+customers.lastname, customers.firstname
+from orders join customers
+using (customer#)
+where orders.shipdate is NULL
+order by orders.shipdate;
+
+
+select books.title, promotion.gift
+from books join PROMOTION
+on books.retail between PROMOTION.MINRETAIL and PROMOTION.MAXRETAIL
+where books.title = 'SHORTEST POEMS';
