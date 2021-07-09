31e8a9f0face0238190686da82410b843b9feb66
Omar Faruk <70847377+omar-faruk01@users.noreply.github.com>
2021-07-09 10:21:29 -0400

    Subquery & Group Func w/ JustLee DB
    Making SQL operations of subqueries and group functions on the JustLee bookstore database

--- Subquery & Group Func/Subquery & Group Func.sql
+++ Subquery & Group Func/Subquery & Group Func.sql
@@ -1,1 +1,30 @@
+select books.RETAIL, category from books where CATEGORY = 'COMPUTER';
 
+select books.RETAIL 
+from books
+where books.RETAIL =
+     (select min(books.RETAIL) from books where books.CATEGORY = 'COMPUTER')
+or books.RETAIL =
+     (select max(books.RETAIL) from books where books.CATEGORY = 'COMPUTER');
+
+
+
+SELECT Category, min(Retail), max(Retail)
+FROM Books
+GROUP BY Category;
+
+SELECT Category, min(Retail), max(Retail)
+FROM Books
+GROUP BY Category
+HAVING min(Retail) > 10;
+
+select max(retail)
+from BOOKS join BOOKAUTHOR using(isbn)
+join author using(AUTHORID)
+where author.lname = 'WHITE'
+and author.fname = 'LISA';
+
+SELECT name, category, AVG(retail), stddev(retail)
+FROM books JOIN publisher USING(pubid)
+WHERE category IN('COMPUTER', 'CHILDREN')
+GROUP BY name, category;
